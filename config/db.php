aa7c3cf262b32928085bc1d3e3c3f0712c4d070b
GabrielSosa <gabrielsosa3093@gmail.com>
2018-04-12 16:02:25 -0600

    Cambio

---
+++ config/db.php
@@ -0,0 +1,24 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'root');//Usuario de tu base de datos
+define('DB_PASS', '');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'internet_sr');//Nombre de la base de datos
+ 
+/*Datos de la empresa*/
+define('NOMBRE_EMPRESA', 'INTERNET SR');
+define('DIRECCION_EMPRESA', 'Tegucigalpa, Col. Hato de Enmedio s-4, b-51, c-3404');
+define('TELEFONO_EMPRESA', '+(504) 2255-4674');
+define('EMAIL_EMPRESA', 'gabrielsosa3093@gmail.com');
+define('TAX', '15');
+
+/*-------------------------------------------------
+Los datos por defecto del usuario administrador son:
+#usuario: admin
+#contraseña: admin
+Los datos de inicio de sesion podran ser modificados 
+desde el panel de control del sistema
+---------------------------------------------------*/
+
+
+?>
